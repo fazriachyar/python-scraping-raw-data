6d79b6eba53bf70ca03d2b9bdfb64f8997539176
fazriachyar <fazriachyar@yandex.com>
2022-06-10 23:43:42 +0700

    second code and it worked

--- index.py
+++ index.py
@@ -2,17 +2,24 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 
-driver = webdriver.Chrome("/usr/lib/chromedriver")
+driver = webdriver.Chrome("/home/fazriachyar/.wdm/drivers/chromedriver/linux64/100.0.4896.60/chromedriver")
 
 products=[]#untuk menyimpan nama produk
 prices=[]#untuk menyimpan harga produk
-driver.get("https://www.flipkart.com/search?q=komputer&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=off&as=off")
+driver.get("https://webscraper.io/test-sites/e-commerce/allinone")
 
 content = driver.page_source
 soup = BeautifulSoup(content)
-for a in soup.findAll('a',href=True, attrs={'class':'_4ddWXP'}):
-    name=a.find('a', attrs={'class':'s1W9rs'})
-    price=a.find('div', attrs={'class':'_30jeq3'})
+# unordered_list = soup.find('div', {'class':'caption'})
+# children = unordered_list.findChildren()
+# for child in children:
+#     print(child)
+
+for a in soup.findAll('div', {'class':'caption'}):
+    name=a.find('a', {'class':'title'})
+    price=a.find('h4', {'class':'pull-right price'})
     products.append(name.text)
     prices.append(price.text)
 
+df = pd.DataFrame({'Product Name':products,'Price':prices}) 
+df.to_csv('products.csv', index=False, encoding='utf-8')
