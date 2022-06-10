dac451fc2b7c4ccc041f445347d3d7b2a56987c6
fazriachyar <fazriachyar@yandex.com>
2022-06-11 00:01:18 +0700

    add comment documentation

--- index.py
+++ index.py
@@ -6,20 +6,20 @@ driver = webdriver.Chrome("/home/fazriachyar/.wdm/drivers/chromedriver/linux64/1
 
 products=[]#untuk menyimpan nama produk
 prices=[]#untuk menyimpan harga produk
-driver.get("https://webscraper.io/test-sites/e-commerce/allinone")
+driver.get("https://webscraper.io/test-sites/e-commerce/allinone")#url website yang akan di scrape
 
-content = driver.page_source
-soup = BeautifulSoup(content)
+content = driver.page_source#mengambil data page_source dari variabel driver
+soup = BeautifulSoup(content)#mulai scrapping variabel content
 # unordered_list = soup.find('div', {'class':'caption'})
 # children = unordered_list.findChildren()
 # for child in children:
 #     print(child)
 
-for a in soup.findAll('div', {'class':'caption'}):
-    name=a.find('a', {'class':'title'})
-    price=a.find('h4', {'class':'pull-right price'})
-    products.append(name.text)
-    prices.append(price.text)
+for a in soup.findAll('div', {'class':'caption'}):#temukan semua div yang memiliki class caption
+    name=a.find('a', {'class':'title'})#temukan semua a yang memiliki class title
+    price=a.find('h4', {'class':'pull-right price'})#temukan semua h4 yang memiliki class pull-right price
+    products.append(name.text)#memasukkan data name hasil scrape ke dalam list product
+    prices.append(price.text)#memasukkan data price hasil scrape ke dalam list prices
 
-df = pd.DataFrame({'Product Name':products,'Price':prices}) 
-df.to_csv('products.csv', index=False, encoding='utf-8')
+df = pd.DataFrame({'Product Name':products,'Price':prices})#membuat kolom Product Name yang berisi isi dari list products dan kolom Price yang berisi variabel prices
+df.to_csv('products.csv', index=False, encoding='utf-8')#import data dalam bentuk .csv
